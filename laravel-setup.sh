0b34788cc0d80dad8d394c515b9ca4a68f98cf58
IvanMasnari <i.masnari@exin.digital>
2022-05-02 11:03:20 +0200

    aggiunto README.md e setup per composer e php

---
+++ laravel-setup.sh
@@ -0,0 +1,10 @@
+#!bin/sh
+
+printf "Installo PHP...\n"
+chmod +x php-setup.sh
+./php-setup.sh
+printf "Installo Composer...\n"
+chmod +x composer-setup.sh
+./composer-setup.sh
+mv composer.phar /usr/local/bin/composer
+printf "Composer installato!"
---
+++ php-setup.sh
@@ -0,0 +1,8 @@
+#!bin/sh
+
+apt-get update
+apt-get upgrade
+apt-get install php
+OUTPUT=$(php --version)
+printf "PHP e' ora correttamente installato nella versione seguente:\n"
+printf $OUTPUT
