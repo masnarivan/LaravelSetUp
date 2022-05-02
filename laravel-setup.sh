6d95bdb43d80d30bfa3ab1789e18f5af5ae6fd7e
IvanMasnari <i.masnari@exin.digital>
2022-05-02 14:39:35 +0200

    aggiunta installazione laravel via composer

--- laravel-setup.sh
+++ laravel-setup.sh
@@ -7,4 +7,14 @@ printf "Installo Composer...\n"
 chmod +x composer-setup.sh
 ./composer-setup.sh
 sudo mv composer.phar /usr/local/bin/composer
-printf "Composer installato!\n"
+printf "Composer installato!\n\n"
+printf "Installo Laravel...\n"
+composer global require laravel/installer
+sudo apt-get install php-curl
+export PATH=~/.config/composer/vendor/bin:$PATH >> .bashrc.sh
+sudo apt install php-xml
+sudo apt-get install php-mbstring
+composer update
+printf "L'installazione ha avuto successo!!"
+
+
